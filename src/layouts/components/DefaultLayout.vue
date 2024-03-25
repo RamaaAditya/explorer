b07a3188d63bc366ed26a9f4c954e67674e735bc
RamaaAditya <project.ramaaditya@gmail.com>
2024-03-25 21:14:10 +0700

    update to DashNode own service

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -80,9 +80,9 @@ dayjs()
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
-          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+          <!-- <img class="w-10 h-10" src="../../../img/dashnode.svg" /> -->
+          <h1 style="color: #f7db05; font-size: 2rem; font-weight: bold; text-align: center; font-family: 'Teko', sans-serif;">
+           DASHNODE SCAN
           </h1>
         </RouterLink>
         <div
